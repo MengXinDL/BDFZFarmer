9b14ec3ee3b5524bdd8cde818318080a5709373d
MengXinDL <bfmmd@foxmail.com>
2024-10-11 22:45:18 +0800

    use esbundle

---
+++ scripts/src/render.ts
@@ -0,0 +1,44 @@
+import { box, boxs, gamecvs } from "./sharedData";
+
+window.onload = function() {
+    resize();
+}
+window.onresize = function() {
+    resize();
+}
+
+const translation: {x: number, y: number, scale: number} = {
+    x: 0,
+    y: 0,
+    scale: 1
+};
+
+function init(): void {
+    let ctx = gamecvs.getContext('2d');
+}
+
+function resize(): void {
+    translation.x = translation.y = 0;
+    translation.scale = 1;
+    gamecvs.width = window.innerWidth;
+    gamecvs.height = window.innerHeight;
+}
+
+function render(): void {
+    let ctx: CanvasRenderingContext2D = gamecvs.getContext('2d') as CanvasRenderingContext2D;
+    let w: number = gamecvs.width, h: number = gamecvs.height;
+
+    ctx.clearRect(0, 0, w, h);
+    ctx.save();
+    
+    ctx.translate(translation.x, translation.y);
+    ctx.scale(translation.scale, translation.scale);
+    boxs.forEach((b: box) => {
+        console.log(1);
+        ctx.fillStyle = b.color;
+        ctx.fillRect(b.x, b.y, b.width, b.height);
+    })
+
+    ctx.restore();
+}
+
---
+++ scripts/src/sharedData.ts
@@ -0,0 +1,20 @@
+export class box {
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+    color: string;
+    constructor(
+        x: number,y: number,
+        width: number,height: number,
+        color: string
+    ){
+        this.x=x;
+        this.y=y;
+        this.width=width;
+        this.height=height;
+        this.color=color;
+    }
+}
+export const gamecvs: HTMLCanvasElement = document.getElementById('game') as HTMLCanvasElement;
+export let boxs: box[] = [];
