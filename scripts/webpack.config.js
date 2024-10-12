058033ee7e2418f2cb75d05fef61a10bc5d81f6b
MengXinDL <bfmmd@foxmail.com>
2024-10-12 13:55:39 +0800

    change into webpack

---
+++ scripts/webpack.config.js
@@ -0,0 +1,22 @@
+let path = require('path');
+
+module.exports = {
+    entry: [
+        './src/index.ts',
+        './src/sharedData.ts',
+        './src/render.ts',
+        './src/interact.ts'
+    ],
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    module: {
+        rules: [
+            { test: /\.ts$/, loader: 'ts-loader' }
+        ]
+    }
+}
